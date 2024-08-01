94d77ec4fab199b14923a821253e3278493e2679
jaylynm <122071154+jaylynm@users.noreply.github.com>
2024-08-01 03:08:53 -0400

    Calendar months, days, events and hover affects Find current year function, update new and prev month function

--- TrackerCalendar.js
+++ TrackerCalendar.js
@@ -1,4 +1,41 @@
+const calendar = document.querySelector(".calendar"),
+    date = document.querySelector(".date"),
+    daysContainer = document.querySelector(".days"),
+    prev = document.querySelector(".prev");
+        next = document.querySelector(".next");
 
-let interestRate = 0.3;
-interestRate =1;
-console.log(interestRate);
+
+let today = new Date();
+let activeDay;
+let month = today.getFullMonth();
+let year = today.getFullYear();
+
+const months = [
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+// function to add days
+
+function initCalendar() {
+    //Update current,prev and next month days
+    const firstDay = new Date(year, month, 1);
+    const lastDay = new Date(year, month + 1, 0);
+    const prevLastDay = new Date(year, month, 0);
+    const prevDays = prevLastDay.getDate();
+    const lastDate = lastDay.getDate();
+    const day = firstDay.getDate();
+    const nextDays = 7 - lastDay.getDay() - 1;
+
+     //Update current date
+        date.innerHTML = months[month] + " " + year;
+}
